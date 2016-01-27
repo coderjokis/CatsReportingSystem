e60c8f27c9fbdd94fc0a9d242b2603919bc0ff52
Joseph Maglalang <joseph.maglalang@robertsoncollege.net>
2016-01-27 09:56:11 -0600

    Added User Class

---
+++ CatsReportingSystem/CatsReportingSystem/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatsReportingSystem.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public User(){}
+        public User(int userID, string userName)
+        {
+            this.UserID = userID;
+            this.UserName = userName;
+        }
+    }
+}
