8174543e8bb193a33f9635e9cee797a3800f2490
Joseph Maglalang <joseph.maglalang@robertsoncollege.net>
2016-01-27 15:36:38 -0600

    Create client class

---
+++ CatsReportingSystem/CatsReportingSystem/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatsReportingSystem.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SIN { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsActive { get; set; }
+
+        public Client(int clientID, string firstName, string lastName, int sIN, DateTime dateOfBirth, bool isActive)
+        {
+            this.ClientID = clientID;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.SIN = sIN;
+            this.DateOfBirth = dateOfBirth;
+            this.IsActive = isActive;
+        }
+    }
+}
